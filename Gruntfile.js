d178bcbae0d098f16d64ea3001ee563cdf95ed38
Alexander Zeilmann <alexander.zeilmann@tngtech.com>
2014-05-14 16:08:53 +0200

    added support for coveralls

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,7 +98,7 @@ module.exports = function (grunt) {
                         statements: 85
                     },
                     root: './lib',
-                    reportFormats: 'lcov'
+                    reportFormats: ['lcov']
                 }
             }
         },
